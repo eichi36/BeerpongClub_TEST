82055694f30729104734673539459e7ffe9bbdeb
eichi36 <47977939+eichi36@users.noreply.github.com>
2020-04-10 10:54:14 +0200

    Update LogInActivity.java
    Testing

--- app/src/main/java/com/example/connectfirebasedemo/LogInActivity.java
+++ app/src/main/java/com/example/connectfirebasedemo/LogInActivity.java
@@ -45,10 +45,6 @@ public class LogInActivity extends AppCompatActivity {
                 Toast.makeText(LogInActivity.this, "Successfully updatet the database", Toast.LENGTH_SHORT).show();
             }
         });
-
-        /*
-        TEST
-                */
     }
 
 
